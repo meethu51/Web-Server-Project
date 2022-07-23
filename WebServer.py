2ae0ce277e8f300660a8086e0060f8e569a0b5d6
meethu51 <85707157+meethu51@users.noreply.github.com>
2022-07-23 20:11:03 +0800

    WebServerPython

---
+++ WebServer.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template
+from flask_socketio import SocketIO
+from flask_socketio import emit
+
+app = Flask(__name__)
+
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+     return render_template('index.html')
+
+@socketio.event
+def ControlUSR0Led(RxData):
+     if RxData == 'on':
+         socketio.emit('ControlUSR0Led', RxData)
+     if RxData == 'off':
+         socketio.emit('ControlUSR0Led', RxData)
+
+@socketio.event
+def BBBW1Event(RxData):
+     socketio.emit('Web_BBBW1Event', RxData)
+     print('Receive Data from BBBW1')
+
+@socketio.event
+def BBBW2Event(RxData):
+     socketio.emit('Web_BBBW2Event', RxData)
+     print('Receive Data from BBBW2')
+
+@socketio.event
+def BBBW3Event(RxData):
+    socketio.emit('Web_BBBW3Event', RxData)
+    print('Receive Data from BBBW3')
+
+@socketio.event
+def BBBW4Event(RxData):
+     socketio.emit('Web_BBBW4Event', RxData)
+     print('Receive Data from BBBW4')
+
+if __name__ == '__main__':
+     app.run(host='192.168.1.136')
